6cda006def0461f5e7f283fbddf314dc9fa9444e
unknown <Admin@.(none)>
2011-08-16 02:04:36 +0400

    create prokect WebClearFusion part#8.3

--- index.php
+++ index.php
@@ -16,10 +16,12 @@
 	if ($config['encoding'] == 'cp1251') $code_page = 'windows-1251';
    		else $code_page = 'utf-8';
 ?>
+	<HTML>
 	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 	<HEAD><link rel="SHORTCUT ICON" href="images/favicon.ico">
 	<TITLE><?php echo  $config['servername']; ?></TITLE>
 	<META http-equiv="content-type" content="text/html; charset=<?php echo  $code_page; ?>" /></HEAD>
+	<BODY>
 <?php
 	if ($config['lang'] == 'en') require "lang/text.".$config['lang'].".php";
               else require "lang/text.".$config['lang'].".".$config['encoding'].".php";
@@ -30,3 +32,7 @@
 	if (file_exists($themefile)) include($themefile);
    		else include("themes/default/theme.php");
 ?>
+	<br><hr width=90%>
+	<CENTER><FONT size=-1><?php echo  $config['copyright']; ?></FONT></CENTER>
+	</BODY></HTML>
+
