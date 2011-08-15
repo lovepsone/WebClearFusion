2604ac286dc7f36fdfe4fd4309385c0a825ba786
unknown <Admin@.(none)>
2011-08-16 03:21:31 +0400

    create prokect WebClearFusion part#8.6

--- include/functions.php
+++ include/functions.php
@@ -12,38 +12,38 @@
 
 	$date = date('d-m-Y [H:i:s]');
 
-	function selectDb($cnt)
+	function selectdb($connect)
 		{
   			global $characters, $mangos, $realmd, $wcf, $encoding;
   
-  			switch ($cnt):
+  			switch ($connect):
   
   			case ("realmd"):
-  			$db = $r_db;
-  			$ip = $r_ip;
-  			$userdb = $r_userdb;
-  			$pw = $r_pw;
+  			$db = $config['rdbName'];
+  			$ip = $config['rhostname'];
+  			$userdb = $config['rusername'];
+  			$pw = $config['rpassword'];
   			break;
   
   			case ("characters"):
-  			$db = $c_db;
-  			$ip = $c_ip;
-  			$userdb = $c_userdb;
-  			$pw = $c_pw;
+  			$db = $config['cdbName'];
+  			$ip = $config['chostname'];
+  			$userdb = $config['cusername'];
+  			$pw = $config['cpassword'];
   			break;
   
  			case ("mangos"):
-  			$db = $m_db;
-  			$ip = $m_ip;
-  			$userdb = $m_userdb;
-  			$pw = $m_pw;
+  			$db = $config['dbName'];
+  			$ip = $config['hostname'];
+  			$userdb = $config['username'];
+  			$pw = $config['password'];
   			break;
   
   			case ("wcf"):
-  			$db = $w_db;
-  			$ip = $w_ip;
-  			$userdb = $w_userdb;
-  			$pw = $w_pw;
+  			$db = $config['wdbName'];
+  			$ip = $config['whostname'];
+  			$userdb = $config['wusername'];
+  			$pw = $config['wpassword'];
   			break;
   
   			endswitch;
