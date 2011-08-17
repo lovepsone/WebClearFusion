3a2062cf29cce10d50088e0b3cff354562edba19
unknown <Admin@.(none)>
2011-08-17 15:26:06 +0400

    wcf_revision_nr = [7] 'Правки по стилям и правки в правой понели + языки'

--- conf.php
+++ conf.php
@@ -59,7 +59,7 @@ $config =array (
 // Ревизия и копирайт wcf (запрещается менять)
 //==================================================================
 'copyright'=>'WebClearFusion v 0.0.8 from LovePSone 2010-2011',
-'revision'=>'wcf_revision_nr = [6]',
+'revision'=>'wcf_revision_nr = [7]',
 );
 
 error_reporting(E_ERROR | E_PARSE | E_WARNING);
--- rightform.php
+++ rightform.php
@@ -68,8 +68,18 @@
   			else $r_act = '0';
 
   			echo"<table width='200' border='0' cellspacing='0' cellpadding='3'>";
-  			echo"<tr><td align='left' valign='top' class='PanelTitle'>$txt[2]</td></tr>";
-  			echo"<tr><td align='right' valign='bottom' class='PanelData'><a href='index.php?modul=acc'>$ra_username</a></td></tr>";
+			echo"<tr><td align='center'>";
+			echo"<table valign='top' border='0' align='right' width='100%' class='PanelBG'>";
+  			echo"<tr><td align='left' valign='top' class='PanelTitle'>$txt[12]</td></tr>";
+  			echo"<tr><td align='right' valign='bottom' class='PanelData'>".ucfirst(strtolower($ra_username))."</td></tr>";
+  			echo"<tr><td align='left' valign='top' class='PanelTitle'>$txt[13]</td></tr>";
+  			echo"<tr><td align='right' valign='bottom' class='PanelData'>$ra_email</td></tr>";
+
+  			echo"<tr><td align='left' valign='top' class='PanelTitle'>IP</td></tr>";
+  			echo"<tr><td align='right' valign='bottom' class='PanelData'>".$_SERVER['REMOTE_ADDR']."</td></tr>";
+
+			echo"<tr><td width='100%' valign='bottom' class='line'></td></tr>";
 			echo"<tr><td align='right' valign='bottom' class='PanelData'><a href='logout.php'>$txt[11]</a></table>";
+			echo"</td></tr></table>";
 		}
 ?>
