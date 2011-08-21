2d568e74c0983a3191e17afdfb0ffbe3f1632118
unknown <Admin@.(none)>
2011-08-21 17:24:16 +0300

    wcf_revision_nr = [19] 'правки в стилях'

--- conf/conf.php
+++ conf/conf.php
@@ -58,8 +58,8 @@ $config =array (
 //==================================================================
 // Ревизия и копирайт wcf (запрещается менять)
 //==================================================================
-'copyright'=>'WebClearFusion v 0.1.31 from LovePSone 2010-2011',
-'revision'=>'wcf_revision_nr = [20]',
+'copyright'=>'WebClearFusion v 0.1.32 from LovePSone 2010-2011',
+'revision'=>'wcf_revision_nr = [21]',
 );
 
 error_reporting(E_ERROR | E_PARSE | E_WARNING);
