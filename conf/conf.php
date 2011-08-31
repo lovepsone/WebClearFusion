87058f4b9d076afe86b59a60a0be3c6e588ddb49
unknown <Admin@.(none)>
2011-08-31 16:35:07 +0300

    wcf_revision_nr = [35] 'чистка в автаризации + раздел форума'

--- conf/conf.php
+++ conf/conf.php
@@ -66,8 +66,8 @@ $config =array (
 //==================================================================
 // Ревизия и копирайт wcf (запрещается менять)
 //==================================================================
-'copyright'=>'WebClearFusion v 0.1.62 from LovePSone 2010-2011',
-'revision'=>'wcf_revision_nr = [34]',
+'copyright'=>'WebClearFusion v 0.1.63 from LovePSone 2010-2011',
+'revision'=>'wcf_revision_nr = [35]',
 );
 
 error_reporting(E_ERROR | E_PARSE | E_WARNING);
