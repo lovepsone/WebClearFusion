e3b8213b37f22fbbeb6b7f9f0dbffe9666fb0f60
unknown <Admin@.(none)>
2012-01-29 13:52:18 +0200

    wcf_revision_nr = [165] 'Adding to the configuration revision ACP. ACP define as a separate project.'

--- conf.php
+++ conf.php
@@ -66,8 +66,9 @@ $config =array (
 // Ревизия и копирайт wcf (запрещается менять)
 //==================================================================
 'copyright'=>'WebClearFusion v 0.4.61 from LovePSone 2010-2011',
-'revision'=>'wcf_revision_nr = [164]',
+'revision'=>'wcf_revision_nr = [165]',
 'rev_admin'=>' 0.02.00',
+'rev_acp'=>' 0.00.10',
 );
 
 define("DB_PREFIX", "wcf_");
