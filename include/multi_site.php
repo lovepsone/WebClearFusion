e154c7ed42858220827ee8d7d3e9a897a92a3b63
unknown <Admin@.(none)>
2012-01-31 23:31:55 +0200

    wcf_revision_nr = [189] 'FILLS CONSTANTS'

--- include/multi_site.php
+++ include/multi_site.php
@@ -18,6 +18,7 @@
 	define("DB_CHR_CLASSES", DB_PREFIX."chr_classes");
 	define("DB_CHR_RACES", DB_PREFIX."chr_races");
 	define("DB_COMMENTS", DB_PREFIX."comments");
+	define("DB_CREATURE_FAMILY", DB_PREFIX."creature_family");
 	define("DB_FORUMS", DB_PREFIX."forums");
 	define("DB_FORUMS_POSTS", DB_PREFIX."forums_posts");
 	define("DB_FORUMS_THREADS", DB_PREFIX."forums_threads");
@@ -28,6 +29,11 @@
 	define("DB_NEWS_CATS", DB_PREFIX."news_cats");
 	define("DB_PANELS", DB_PREFIX."panels");
 	define("DB_SETTINGS", DB_PREFIX."settings");
+	define("DB_SPELL", DB_PREFIX."spell");
+	define("DB_SPELL_DURATION", DB_PREFIX."spell_duration");
+	define("DB_TALENT_TAB", DB_PREFIX."talent_tab");
+	define("DB_TALENTS", DB_PREFIX."talents");
+	define("DB_SPELL_ICON", DB_PREFIX."spellicon");
 	define("DB_USERS", DB_PREFIX."users");
 
 	//===============================================
