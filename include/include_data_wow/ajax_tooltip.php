bce4a0c1433fb4083ea0d45b8a59d4f0d8e8b4ec
lovepsone <lovepsone@gmail.com>
2012-02-26 15:59:16 +0000

    wcf_revision_nr = [252] 'Add tooltip'

--- include/include_data_wow/ajax_tooltip.php
+++ include/include_data_wow/ajax_tooltip.php
@@ -17,27 +17,55 @@
 		{
 			// Показ вещи
 			case "i":
-			if (substr($str,1,1) == 'g')
-				{
-					$entry = intval(substr($str,2,10));
-					if ($item_data = get_item_data($entry))
-						{
-							if ($item = get_item($item_data[ITEM_FIELD_ENTRY])) { no_border_item_table($item, $item_data); }
-						}
-	 				else
-						{
-							echo "Error item guid $entry";
-						}
- 				}
-
-			elseif ($item = get_item($entry))
-				{
-					no_border_item_table($item,0,0);
-				}
-			else
-				{
-					echo "Error item $entry";
-				}
+				if (substr($str,1,1) == 'g')
+					{
+						$entry = intval(substr($str,2,10));
+						if ($item_data = get_item_data($entry))
+							{
+								if ($item = get_item($item_data[ITEM_FIELD_ENTRY])) { no_border_item_table($item, $item_data); }
+							}
+		 				else
+							{
+								echo "Error item guid $entry";
+							}
+	 				}
+	
+				elseif ($item = get_item($entry))
+					{
+						no_border_item_table($item,0,0);
+					}
+				else
+					{
+						echo "Error item $entry";
+					}
 			break;
-}
+
+			// Показ спелла
+			case "s":
+				if ($spell=get_spell($entry))
+					{
+						no_border_spell_table($spell);
+					}
+				else
+					{
+						echo "Error spell $entry";
+					}
+  			break;
+
+			// Показ таланта
+			case "t":
+				$rank  = intval(substr($str,1,1));
+				$entry = intval(substr($str,2,5));
+				selectdb("wcf");
+				$talentTab = db_assoc(db_query("SELECT * FROM ".DB_TALENTS." WHERE `TalentID`='$entry'"));
+				if ($talentTab)
+					{
+						no_border_talent_table($talentTab, $rank);
+					}
+				else
+					{
+						echo "Error talent $entry - $rank";
+					}
+			break;
+	}
 ?>
