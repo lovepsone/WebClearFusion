60e21d6b32e5ead1b91bbe9d1e08b27d9249b63c
lovepsone <lovepsone@gmail.com>
2012-03-13 21:49:56 +0300

    wcf_revision_nr = [276] 'add wow content part#3'
    Signed-off-by: lovepsone <lovepsone@gmail.com>

---
+++ contentwow/talent.php
@@ -0,0 +1,72 @@
+<?php
+/*-------------------------------------------------------+
+| WebClearFusion Content Management System
+| Copyright (C) 2010 - 2012 lovepsone
++--------------------------------------------------------+
+| Filename: talent.php
+| Author: lovepsone
++--------------------------------------------------------+
+| Removal of this copyright header is strictly prohibited 
+| without written permission from the original author(s).
++--------------------------------------------------------*/
+
+	//==============================================================================
+	// Скрипт предназначен для вывода талантов игрока
+	//==============================================================================
+
+	$talent = (isset($_REQUEST['talent']) ? strtolower(@$_REQUEST['talent']) : "");
+	if (isset($_REQUEST['pet'])) { $pid = intval($_REQUEST['pet']); } else { $pid = -1; }
+	$bild  = (isset($_REQUEST['bild']) ? @$_REQUEST['bild'] : "");
+	$cid = 0;
+	$link = '?talent';
+	$header = '';
+
+	opentable();
+	echo"<div align='center'>";
+
+	$cname = array(1=>'warrior',2=>'paladin',3=>'hunter',4=>'rogue',5=>'priest',6=>'death_knight',7=>'shaman',8=>'mage',9=>'warlock',11=>'druid');
+
+	foreach($cname as $c=>$name)
+		{
+			echo'<a href="?talent='.$name.'" '.add_tooltip(get_classes($c)).'><img class=item src="'.get_class_image($c).'"></a>&nbsp;';
+
+			if ($talent==$name)
+				{
+					$header = get_classes($c);
+					$link.="=".$name;
+					$cid = $c;
+				}
+		}
+	echo"<br>";
+
+	selectdb("wcf");
+	$result = db_query("SELECT `id`, `category` FROM ".DB_CREATURE_FAMILY." WHERE `category` <> -1 ORDER BY `name`");
+	while ($data = db_array($result))
+		{
+			$f = $data['id'];
+			$c = $data['category'];
+			echo'<a href="?talent&pet='.$c.'" '.add_tooltip(get_creature_family($f,0)).'><img class=item src="'.get_family_image($f).'"></a>&nbsp;';
+			if ($pid==$c)
+				{
+					$header = get_creature_family($f);
+					$link.="&pet=".$c;
+				}
+		}
+	echo"</div><br>";
+
+
+
+	if ($cid OR $pid >= 0)
+		{
+			echo"<div id='talent' align='center'>";
+			include_talent_script($cid, $pid, $config['talent_calc_max_level'], $header);
+			if ($bild) echo'<script type="text/javascript">tc_bildFromStr("'.$bild.'");</script>';
+			echo'<script type="text/javascript">tc_renderTree("talent");</script></div>';
+		}
+
+	echo'<div class=faq><center>';
+	echo'<a href="'.$link.'" id=talent_bild_link>Bild link</a> | <a href="#" onclick="return tc_resetBild();">Reset talents</a>';
+	echo'</center></div>';
+	closetable();
+
+?>
--- online.php
+++ online.php
@@ -13,101 +13,10 @@
 	require_once "maincore.php";
 	require_once THEMES."templates/header.php";
 
-	selectdb("realmd");
-	$result_r = db_query("SELECT * FROM `realmlist`");
-
-	if (!isset($_GET['realm_id']) && !isnum($_GET['realm_id']))
+	if ($config['type_server'] = '1' || $config['type_server'] = '2')
 		{
-			redirect(WCF_SELF."?realm_id=".$config['defult_realmd_id']);
+			require_once CONTENT_WOW."online.php";
 		}
-	elseif (isset($_GET['realm_id']) && isnum($_GET['realm_id']))
-		{
-			$realm_id = addslashes($_GET["realm_id"]);
-			opentable();
-			if ($config['namber_realmd'] > 1)
-				{
-					echo"<tr><td align='center' colspan='6'><ul class='my_tabs'><center>";
-					while ($data_r = db_array($result_r)) { echo"<li><a onclick='return uploadFromHref(this, \"reportContainer\");' href='".WCF_SELF."?realm_id=".$data_r['id']."'>".ucfirst(strtolower($data_r['name']))."</a></li>"; }
- 					echo"</center></ul>";
-				}
 
-			$result = db_query("SELECT `address`, `port` FROM `realmlist` WHERE `id`='$realm_id'");
-			$data = db_assoc($result);
-
-			echo"<tr><td align='center' colspan='6'>";
-			if (@fsockopen($data['address'], $data['port'], $errno, $errstr, 1)) { echo"<img src='".IMAGES."online.gif' align='absmiddle' alt='online'>"; } else { echo"<img src='".IMAGES."offline.gif' align='absmiddle' alt='ofline'>"; }
-			echo"</td></tr>";
-
-			selectdb("characters");
-			$result1 = db_query("SELECT * FROM `saved_variables`");
-
-			if ($data1 = db_assoc($result1))
-				{
-					$ap_date = date("H:i:s d.m.Y", $data1['NextArenaPointDistributionTime']);
-					$daily_quest_date = date("H:i:s d.m.Y", $data1['NextDailyQuestResetTime']);
-					$weekly_quest_date = date("H:i:s d.m.Y", $data1['NextWeeklyQuestResetTime']);
-					$monthly_quest_date = date("H:i:s d.m.Y", $data1['NextMonthlyQuestResetTime']);
-
-					echo"<tr><td align='center' colspan='6' class='tbl'>".$txt['modul_online_timers']."</td></tr>";
-					echo"<tr><td colspan='6'><table width='100%'>";
-					echo"<tr><td align='right' class='tbl1'>".$txt['modul_online_points_arena']."</td>";
-					echo"<td align='left' class='tbl1'>".$ap_date."</td></tr>";
-					echo"<tr><td align='right' class='tbl1'>".$txt['modul_online_daily_quests']."</td>";
-					echo"<td align='left' class='tbl1'>".$daily_quest_date."</td></tr>";
-					echo"<tr><td align='right' class='tbl1'>".$txt['modul_online_weekly_quests']."</td>";
-					echo"<td align='left' class='tbl1'>".$weekly_quest_date."</td></tr>";
-					echo"<tr><td align='right' class='tbl1'>".$txt['modul_online_monthly_quests']."</td>";
-					echo"<td align='left' class='tbl1'>".$monthly_quest_date."</td></tr>";
-					echo"</td></tr></table>";
-				}
-
-			$result2 = db_query("SELECT count(`guid`) as kol FROM `characters` WHERE `online` = 1");
-			$kolzap = mysql_fetch_array($result2);
-
-			if ($kolzap['kol'] > $config['page_online'])
-    				{
-    					$page_len_online = $config['page_online'];
-    					if (!isset($_GET['page']) or ($_GET['page'] == "")) { $start_rec_online = 0; } else { $start_rec_online = ((int) $_GET['page'] - 1)*$config['page_online']; }
-    				}
-			else
-    				{
-    					$page_len_online = $kolzap['kol'];
-					$start_rec_online = 0;
-    				}
-			$result3 = db_query("SELECT * FROM `characters` WHERE `online` = 1 ORDER BY `name` LIMIT ".$start_rec_online.",".$page_len_online);
-
-			if (db_num_rows($result3))
-				{
-					$online = db_num_rows($result3);
-					echo"<tr><td align='center' colspan='6' class='tbl1'>".$online." ".$txt['modul_online']."</td></tr>";
-					echo"<tr><td width='1%' class='tbl'>".$txt['modul_online_level']."</td>";
-					echo"<td width='1%' class='tbl'></td>";
-					echo"<td align='center' class='tbl'>".$txt['modul_online_name']."</td>";
-					echo"<td width='1%' align='right' class='tbl'>".$txt['modul_online_race']."</td>";
-					echo"<td width='1%' align='right' class='tbl'>".$txt['modul_online_class']."</td>";
-					echo"<td width='1%' align='right' class='tbl'>".$txt['modul_online_position']."</td></tr>";
-
-					while ($data3 = db_array($result3))
-						{
-							echo"<tr><td width='1%' align='center' class='tbl1'>".$data3['level']."</td>";
-							echo"<td width='1%' class='tbl1'>".get_faction_image($data3['race'])."</td>";
-							echo"<td align='center' class='tbl1'>".$data3['name']."</td>";
-							echo"<td width='1%' class='tbl1'><img width='20' src='".get_race_image($data3['race'],$data3['gender'])."'></td>";
-							echo"<td width='1%' class='tbl1'><img width='20' src='".get_class_image($data3['class'])."'></td>";
-							echo"<td width='1%' class='tbl1'>".$zones[$data3['zone']]."</td></tr>";
-						}
-					if ($kolzap['kol'] > $config['page_online'])
-						{
-							$page_counter = ceil($kolzap['kol'] / $config['page_online']);
-					    		if (!isset($_GET['page']) OR ($_GET['page'] == '') OR ($_GET['page'] == '_')) { $tp3 = 1; } else { $tp3 = (int)$_GET['page']; }
-					    		echo"<tr><th height='40' colspan='6' align='center'>".show_page(WCF_SELF.'?realm_id='.$realm_id.'&page=', $tp3, $page_counter)."</th></tr>";
-					    }
-				}
-			else
-				{
-					echo"<tr><td align='center' colspan='6' class='tbl1'>".$txt['modul_online_no_char']."</td></tr>";
-				}
-			closetable();
-		}
 	require_once THEMES."templates/footer.php";
 ?>
