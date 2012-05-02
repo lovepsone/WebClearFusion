d4aed58b3110fbd0d6235684352ae0074dcb1cab
lovepsone <lovepsone@gmail.com>
2012-05-03 00:39:35 +0300

    wcf_revision_nr = [325] 'Remove unnecessary files'
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- conf.php
+++ conf.php
@@ -33,7 +33,7 @@
 	// Ревизия и копирайт wcf (запрещается менять)
 	//==================================================================
 	$config['copyright'] = 'WebClearFusion v 0.4.73 from LovePSone 2010-2011';
-	$config['revision'] = 'wcf_revision_nr = [324]';
+	$config['revision'] = 'wcf_revision_nr = [326]';
 	$config['rev_admin'] = ' 0.02.00';
 	$config['rev_acp'] = ' 0.02.00';
 
