3c27897fd091f09c9aa0553da8cfed657645afd2
lovepsone <lovepsone@mail.ru>
2013-08-20 16:02:08 +0300

    wcf_revision_nr = [379] 'fixed notise in cat_img'

--- news.php
+++ news.php
@@ -18,23 +18,23 @@
 	$kolzap = db_array($result);
 
 	if ($kolzap['number'] > $config['page_news'])
-		{
-    			$page_len = $config['page_news'];
+	{
+    		$page_len = $config['page_news'];
  
-    			if (!isset($_GET['page']) or ($_GET['page'] == ''))
-				{
-					$start_rec = 0;
-				}
-			else
-				{
-					$start_rec = ((int)$_GET['page']-1)*$config['page_news'];
-				}
-		}
-	else
+    		if (!isset($_GET['page']) or ($_GET['page'] == ''))
 		{
-    			$page_len = $kolzap['number'];
 			$start_rec = 0;
 		}
+		else
+		{
+			$start_rec = ((int)$_GET['page']-1)*$config['page_news'];
+		}
+	}
+	else
+	{
+    		$page_len = $kolzap['number'];
+		$start_rec = 0;
+	}
 
   	$result = db_query("SELECT * FROM ".DB_NEWS." 
 				LEFT JOIN ".DB_NEWS_CATS." ON `news_cat_id`=`news_cat`
@@ -43,43 +43,44 @@
 
 	opentable();
   	if (db_num_rows($result))
+	{
+     		while ($data = db_array($result))
 		{
-     			while ($data = db_array($result))
+			if (check_user($data['news_visibility']))
+			{
+				$news_cat_image = "";
+				if ($data['news_show_cat'] == 1)
 				{
-					if (check_user($data['news_visibility']))
-						{
-							if ($data['news_show_cat'] == 1)
-								{
-									$img_size = @getimagesize(IMAGES_NC.$data['news_cat_image']);
-									$news_cat_image = "<img src='".IMAGES_NC.$data['news_cat_image']."' width='".$img_size[0]."' height='".$img_size[1]."' class='news-category' />";
-								}
+					$img_size = @getimagesize(IMAGES_NC.$data['news_cat_image']);
+					$news_cat_image = "<img src='".IMAGES_NC.$data['news_cat_image']."' width='".$img_size[0]."' height='".$img_size[1]."' class='news-category' />";
+				}	
 
-          						echo"<tr><td align='left' class='head-table'>".$data['news_subject']."</td></tr>";
-							echo"<tr><td align='left'>".$news_cat_image.stripslashes($data['news_text'])."</td></tr>";
-          						echo"<tr><td align='center'><br>".$txt['modul_news_creation_date']."&nbsp;".$data['news_date']."&nbsp;".$txt['modul_news_author']."&nbsp;
-								".ucfirst(strtolower($data['user_name']))."&nbsp;|&nbsp;<a href='newsext.php?id=".$data['news_id']."'>".$txt['modul_news_read_more']."</a><br><hr></td></tr>";
-						}
-      				}
+          			echo"<tr><td align='left' class='head-table'>".$data['news_subject']."</td></tr>";
+				echo"<tr><td align='left'>".$news_cat_image.stripslashes($data['news_text'])."</td></tr>";
+          			echo"<tr><td align='center'><br>".$txt['modul_news_creation_date']."&nbsp;".$data['news_date']."&nbsp;".$txt['modul_news_author']."&nbsp;
+						".ucfirst(strtolower($data['user_name']))."&nbsp;|&nbsp;<a href='newsext.php?id=".$data['news_id']."'>".$txt['modul_news_read_more']."</a><br><hr></td></tr>";
+			}
+      		}
 
-  			if ($kolzap['number'] > $config['page_news'])
-				{
-  					$PageCounter = ceil($kolzap['number'] / $config['page_news']);
+  		if ($kolzap['number'] > $config['page_news'])
+		{
+  			$PageCounter = ceil($kolzap['number'] / $config['page_news']);
 
-   					if (!isset($_GET['page']) OR ($_GET['page'] == '') OR ($_GET['page'] == '_'))
-						{
-							$tp3 = 1;
-						}
-					else
-						{
-							$tp3 = (int)$_GET['page'];
-						}
- 					echo"<tr><td height='30' align='center'>".show_page(WCF_SELF.'?page=',$tp3,$PageCounter)."</td></tr>";
-  				}
-   		}
+   			if (!isset($_GET['page']) OR ($_GET['page'] == '') OR ($_GET['page'] == '_'))
+			{
+				$tp3 = 1;
+			}
+			else
+			{
+				$tp3 = (int)$_GET['page'];
+			}
+ 			echo"<tr><td height='30' align='center'>".show_page(WCF_SELF.'?page=',$tp3,$PageCounter)."</td></tr>";
+  		}
+   	}
 	else
-		{
-			echo"<tr><td align='center' valign='middle' >".$txt['modul_news_no_news']."</td></tr>";
-		}
+	{
+		echo"<tr><td align='center' valign='middle' >".$txt['modul_news_no_news']."</td></tr>";
+	}
 	closetable();
 
 	require_once THEMES."templates/footer.php";
