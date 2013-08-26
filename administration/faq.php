1c3f440f5b8768ae52d82486850644fa35b095f1
lovepsone <lovepsone@mail.ru>
2013-08-26 11:43:30 +0300

    wcf_revision_nr = [392] '#9 The transition to the use of the class, need tested'

--- administration/faq.php
+++ administration/faq.php
@@ -3,7 +3,7 @@
 | WebClearFusion Content Management System
 | Copyright (C) 2010 - 2013 lovepsone
 +--------------------------------------------------------+
-| Filename: forumedit.php
+| Filename: faq.php
 | Author: lovepsone
 +--------------------------------------------------------+
 | Removal of this copyright header is strictly prohibited 
@@ -15,133 +15,135 @@
    	require_once INCLUDES."tinymce.php";
    	echo $advanced_script;
 
-	if ((isset($_GET['action']) && $_GET['action'] == "delete") && (isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
-		{
-			selectdb("wcf");
-			$result = db_count("(faq_cat_id)", DB_FAQS, "faq_cat_id='".$_GET['faq_cat_id']."'");
+	opentable();
+	echo "Section faq for administratirovaniya temporarily closed";
+	closetable();
+	/*if ((isset($_GET['action']) && $_GET['action'] == "delete") && (isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
+	{
+		$result = db_count("(faq_cat_id)", DB_FAQS, "faq_cat_id='".$_GET['faq_cat_id']."'");
 
-			if (!empty($result))
-				{
-					redirect(WCF_SELF."&status=delcn");
-				}
-			else
-				{
-					$result = db_query("DELETE FROM ".DB_FAQ_CATS." WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
-					redirect(WCF_SELF."?status=delcy");
-				}
+		if (!empty($result))
+		{
+			redirect(WCF_SELF."&status=delcn");
 		}
+		else
+		{
+			$result = db_query("DELETE FROM ".DB_FAQ_CATS." WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
+			redirect(WCF_SELF."?status=delcy");
+		}
+	}
 	elseif ((isset($_GET['action']) && $_GET['action'] == "delete") && (isset($_GET['faq_id']) && isnum($_GET['faq_id'])) && (isset($_GET['t']) && $_GET['t'] == "faq"))
+	{
+		selectdb("wcf");
+		$faq_count = db_count("(faq_id)", DB_FAQS, "faq_id='".$_GET['faq_id']."'");
+		$result = db_query("DELETE FROM ".DB_FAQS." WHERE `faq_id`='".$_GET['faq_id']."'");
+		if ($faq_count)
 		{
-			selectdb("wcf");
-			$faq_count = db_count("(faq_id)", DB_FAQS, "faq_id='".$_GET['faq_id']."'");
-			$result = db_query("DELETE FROM ".DB_FAQS." WHERE `faq_id`='".$_GET['faq_id']."'");
-			if ($faq_count)
-				{
-					redirect(WCF_SELF."?faq_cat_id=".intval($_GET['faq_cat_id'])."&status=del");
-				}
-			else
-				{
-					redirect(WCF_SELF."?status=del");
-				}
+			redirect(WCF_SELF."?faq_cat_id=".intval($_GET['faq_cat_id'])."&status=del");
 		}
+		else
+		{
+			redirect(WCF_SELF."?status=del");
+		}
+	}
 	elseif (isset($_POST['save_cat']))
-		{
-			selectdb("wcf");
-			$faq_cat_name = stripinput($_POST['faq_cat_name']);
-			$faq_cat_description = stripinput($_POST['faq_cat_description']);
+	{
+		selectdb("wcf");
+		$faq_cat_name = stripinput($_POST['faq_cat_name']);
+		$faq_cat_description = stripinput($_POST['faq_cat_description']);
 
-			if ($faq_cat_name)
-				{
-					if ((isset($_GET['action']) && $_GET['action'] == "edit") && (isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
-						{
-							$result = db_query("UPDATE ".DB_FAQ_CATS." SET `faq_cat_name`='$faq_cat_name', `faq_cat_description`='$faq_cat_description' WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
-							redirect(WCF_SELF."?status=scu");
-						}
-					else
-						{
-							$result = db_query("INSERT INTO ".DB_FAQ_CATS." (`faq_cat_name`, `faq_cat_description`) VALUES('$faq_cat_name', '$faq_cat_description')");
-							redirect(WCF_SELF."?status=scn");
-						}
-				}
+		if ($faq_cat_name)
+		{
+			if ((isset($_GET['action']) && $_GET['action'] == "edit") && (isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
+			{
+				$result = db_query("UPDATE ".DB_FAQ_CATS." SET `faq_cat_name`='$faq_cat_name', `faq_cat_description`='$faq_cat_description' WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
+				redirect(WCF_SELF."?status=scu");
+			}
 			else
-				{
-					redirect(WCF_SELF);
-				}
+			{
+				$result = db_query("INSERT INTO ".DB_FAQ_CATS." (`faq_cat_name`, `faq_cat_description`) VALUES('$faq_cat_name', '$faq_cat_description')");
+				redirect(WCF_SELF."?status=scn");
+			}
 		}
+		else
+		{
+			redirect(WCF_SELF);
+		}
+	}
 	elseif (isset($_POST['save_faq']))
-		{
-			selectdb("wcf");
-			$faq_cat = intval($_POST['faq_cat']);
-			$faq_question = stripinput($_POST['faq_question']);
-			$faq_answer = addslash($_POST['faq_answer']);
+	{
+		selectdb("wcf");
+		$faq_cat = intval($_POST['faq_cat']);
+		$faq_question = stripinput($_POST['faq_question']);
+		$faq_answer = addslash($_POST['faq_answer']);
 
-			if ($faq_question && $faq_answer)
-				{
-					if ((isset($_GET['action']) && $_GET['action'] == "edit") && (isset($_GET['faq_id']) && isnum($_GET['faq_id'])) && (isset($_GET['t']) && $_GET['t'] == "faq"))
-						{
-							$result = db_query("UPDATE ".DB_FAQS." SET `faq_cat_id`='$faq_cat', `faq_question`='$faq_question', `faq_answer`='$faq_answer' WHERE `faq_id`='".$_GET['faq_id']."'");
-							redirect(WCF_SELF."?faq_cat_id=$faq_cat&status=su");
-						}
-					else
-						{
-							$result = db_query("INSERT INTO ".DB_FAQS." (`faq_cat_id`, `faq_question`, `faq_answer`) VALUES ('$faq_cat', '$faq_question', '$faq_answer')");
-							redirect(WCF_SELF."?faq_cat_id=$faq_cat&status=sn");
-						}
-				}
+		if ($faq_question && $faq_answer)
+		{
+			if ((isset($_GET['action']) && $_GET['action'] == "edit") && (isset($_GET['faq_id']) && isnum($_GET['faq_id'])) && (isset($_GET['t']) && $_GET['t'] == "faq"))
+			{
+				$result = db_query("UPDATE ".DB_FAQS." SET `faq_cat_id`='$faq_cat', `faq_question`='$faq_question', `faq_answer`='$faq_answer' WHERE `faq_id`='".$_GET['faq_id']."'");
+				redirect(WCF_SELF."?faq_cat_id=$faq_cat&status=su");
+			}
 			else
+			{
+				$result = db_query("INSERT INTO ".DB_FAQS." (`faq_cat_id`, `faq_question`, `faq_answer`) VALUES ('$faq_cat', '$faq_question', '$faq_answer')");
+				redirect(WCF_SELF."?faq_cat_id=$faq_cat&status=sn");
+			}
+		}
+		else
+		{
+			redirect(WCF_SELF);
+		}
+	}
+	elseif (isset($_GET['action']) && $_GET['action'] == "edit")
+	{
+		selectdb("wcf");
+		if ((isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
+		{
+			$result = db_query("SELECT `faq_cat_id`, `faq_cat_name`, `faq_cat_description` FROM ".DB_FAQ_CATS." WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
+			if (db_num_rows($result))
+			{
+				$data = db_assoc($result);
+				$faq_cat_id = $data['faq_cat_id'];
+				$faq_cat_name = $data['faq_cat_name'];
+				$faq_cat_description = $data['faq_cat_description'];
+				$faq_cat_title = WCF::$locale['admin_faq_edit_cats'];
+				$faq_cat_action = WCF_SELF."?action=edit&faq_cat_id=".$data['faq_cat_id']."&t=cat";
+				// --------------------- //
+				$faq_question = "";
+				$faq_answer = "";
+				$faq_title = WCF::$locale['admin_faq_add_cats'];
+				$faq_action = WCF_SELF;
+			}
+			else
+			{
+				redirect(WCF_SELF);
+			}
+		}
+		elseif ((isset($_GET['faq_id']) && isnum($_GET['faq_id'])) && (isset($_GET['t']) && $_GET['t'] == "faq"))
+		{
+			$result = db_query("SELECT `faq_id`, `faq_question`, `faq_answer` FROM ".DB_FAQS." WHERE `faq_id`='".$_GET['faq_id']."'");
+			if (db_num_rows($result))
+			{
+				$data = db_assoc($result);
+				$faq_cat_name = "";
+				$faq_cat_description = "";
+				$faq_cat_title = WCF::$locale['420'];
+				$faq_cat_action = WCF_SELF;
+				// --------------------- //
+				$faq_id = $data['faq_id'];
+				$faq_question = $data['faq_question'];
+				$faq_answer = stripslashes($data['faq_answer']);
+				$faq_title = WCF::$locale['501'];
+				$faq_action = WCF_SELF."?action=edit&faq_id=".$data['faq_id']."&t=faq";
+				}
+				else
 				{
 					redirect(WCF_SELF);
 				}
+			}
 		}
-	elseif (isset($_GET['action']) && $_GET['action'] == "edit")
-		{
-			selectdb("wcf");
-			if ((isset($_GET['faq_cat_id']) && isnum($_GET['faq_cat_id'])) && (isset($_GET['t']) && $_GET['t'] == "cat"))
-				{
-					$result = db_query("SELECT `faq_cat_id`, `faq_cat_name`, `faq_cat_description` FROM ".DB_FAQ_CATS." WHERE `faq_cat_id`='".$_GET['faq_cat_id']."'");
-					if (db_num_rows($result))
-						{
-							$data = db_assoc($result);
-							$faq_cat_id = $data['faq_cat_id'];
-							$faq_cat_name = $data['faq_cat_name'];
-							$faq_cat_description = $data['faq_cat_description'];
-							$faq_cat_title = WCF::$locale['admin_faq_edit_cats'];
-							$faq_cat_action = WCF_SELF."?action=edit&faq_cat_id=".$data['faq_cat_id']."&t=cat";
-							// --------------------- //
-							$faq_question = "";
-							$faq_answer = "";
-							$faq_title = WCF::$locale['admin_faq_add_cats'];
-							$faq_action = WCF_SELF;
-						}
-					else
-						{
-							redirect(WCF_SELF);
-						}
-				}
-			elseif ((isset($_GET['faq_id']) && isnum($_GET['faq_id'])) && (isset($_GET['t']) && $_GET['t'] == "faq"))
-				{
-					$result = db_query("SELECT `faq_id`, `faq_question`, `faq_answer` FROM ".DB_FAQS." WHERE `faq_id`='".$_GET['faq_id']."'");
-					if (db_num_rows($result))
-						{
-							$data = db_assoc($result);
-							$faq_cat_name = "";
-							$faq_cat_description = "";
-							$faq_cat_title = WCF::$locale['420'];
-							$faq_cat_action = WCF_SELF;
-							// --------------------- //
-							$faq_id = $data['faq_id'];
-							$faq_question = $data['faq_question'];
-							$faq_answer = stripslashes($data['faq_answer']);
-							$faq_title = WCF::$locale['501'];
-							$faq_action = WCF_SELF."?action=edit&faq_id=".$data['faq_id']."&t=faq";
-						}
-					else
-						{
-							redirect(WCF_SELF);
-						}
-				}
-		}
-	else
+		else
 		{
 			$faq_cat_name = "";
 			$faq_cat_description = "";
@@ -154,21 +156,21 @@
 		}
 
 	if (!isset($_GET['t']) || $_GET['t'] != "faq")
-		{
-			opentable();
-			echo"<form name='add_faq_cat' method='post' action='".$faq_cat_action."'>";
-			echo"<tr><td align='center' colspan='2' class='small'>".$faq_cat_title."</td></tr>";
+	{
+		opentable();
+		echo"<form name='add_faq_cat' method='post' action='".$faq_cat_action."'>";
+		echo"<tr><td align='center' colspan='2' class='small'>".$faq_cat_title."</td></tr>";
 
-			echo"<tr><td width='50%' align='right' class='small2'>".WCF::$locale['admin_faq_name_cats']."&nbsp;</td>";
-			echo"<td align='left'><input type='text' name='faq_cat_name' value='".$faq_cat_name."' class='textbox' style='width:210px' /></td></tr>";
+		echo"<tr><td width='50%' align='right' class='small2'>".WCF::$locale['admin_faq_name_cats']."&nbsp;</td>";
+		echo"<td align='left'><input type='text' name='faq_cat_name' value='".$faq_cat_name."' class='textbox' style='width:210px' /></td></tr>";
 
-			echo"<tr><td width='50%' align='right' class='small2'>".WCF::$locale['admin_faq_description']."&nbsp;</td>";
-			echo"<td align='left'><input type='text' name='faq_cat_description' value='".$faq_cat_description."' class='textbox' style='width:250px;' /></td></tr>";
+		echo"<tr><td width='50%' align='right' class='small2'>".WCF::$locale['admin_faq_description']."&nbsp;</td>";
+		echo"<td align='left'><input type='text' name='faq_cat_description' value='".$faq_cat_description."' class='textbox' style='width:250px;' /></td></tr>";
 
-			echo"<tr><td align='center' colspan='2'><input type='submit' name='save_cat' value='".WCF::$locale['admin_faq_save']."' class='button' /></td></tr>";
-			echo"</form>";
-			closetable();
-		}
+		echo"<tr><td align='center' colspan='2'><input type='submit' name='save_cat' value='".WCF::$locale['admin_faq_save']."' class='button' /></td></tr>";
+		echo"</form>";
+		closetable();
+	}
 
 	if (!isset($_GET['t']) || $_GET['t'] != "cat")
 		{
@@ -258,7 +260,7 @@
 		{
 			echo"<div style='text-align:center'>".WCF::$locale['admin_faq_no_cats']."<br /></div>";
 		}
-	closetable();
+	closetable();*/
 
 	require_once THEMES."templates/footer.php";
 ?>
--- include/revision_nr.php
+++ include/revision_nr.php
@@ -1,5 +1,5 @@
 <?php
-define('WCF_REVISION', 391);
+define('WCF_REVISION', 392);
 define('DB_VERSION', 'wcf_r350');
 define('CONFIG_VERSION', '291120122');
 ?>
